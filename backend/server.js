b1d8e97deeb3cd38e353e0e036fff09d5ea0a9f9
danielziva1 <danielziva9@gmail.com>
2020-12-07 22:22:14 +0200

    six try

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,9 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
 
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
+app.use('/api/  ', productRouter);
 app.use('/api/orders', orderRouter);
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
@@ -36,9 +34,18 @@ app.use((err, req, res, next) => {
 const __dirname = path.resolve();
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
+
+
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-});
+}); 
 app.get('/api/config/google', (req, res) => {
   res.send(process.env.GOOGLE_API_KEY || '');
 });
