279ddd2f85905f51ce85ae00f11f67ca8ed483ea
danielziva1 <danielziva9@gmail.com>
2020-12-08 00:10:19 +0200

    8 try

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,12 @@
-
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
-import dotenv from 'dotenv';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
-import path from 'path';
-
-
-
-
 dotenv.config();
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,19 +15,16 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
-
-app.use('/api/orders', orderRouter);
 app.use('/api/products', productRouter);
-
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 const __dirname = path.resolve();
-
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
@@ -42,12 +33,8 @@ app.get('*', (req, res) =>
 //   res.send('Server is ready');
 // });
 
-
-app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-}); 
-app.get('/api/config/google', (req, res) => {
-  res.send(process.env.GOOGLE_API_KEY || '');
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
