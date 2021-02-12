b98764c135c61987bda495a3ddf69b614309c7b1
danielziva1 <danielziva9@gmail.com>
2021-02-12 10:32:07 +0200

    try f

--- frontend/src/screens/aboutScreen.js
+++ frontend/src/screens/aboutScreen.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
 
-let  d ='/images/p7.jpg'
-let  e ='/images/dddd.jpg'
-let  a ='/images/p5.jpg'
+
 
 export class aboutScreen extends Component {
 
@@ -13,17 +9,6 @@ export class aboutScreen extends Component {
           <div className='g'>
 
 
-              <Carousel    showArrows autoPlay showThumbs={false}>
-             
-                   
-                      <img src={d} alt={d}  width="240"  />
-                      <img src={d} alt={d} />
-                      <img src={d} alt={d} />
-               
-                
-             
-              </Carousel>
-              
           
   
             </div>
